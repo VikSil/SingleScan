71c7a352cbf7839bf6cc1cb3a714b1b188f9c28a
Viktoria Silver <viktoria.sudraba@gmail.com>
2025-09-29 23:08:37 +0100

    First working commit - router and camera installed

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { CameraView, Camera } from "expo-camera";
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
