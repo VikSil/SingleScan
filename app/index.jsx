d459018f6b1f6111498803ea1babab5184a86018
Viktoria Silver <viktoria.sudraba@gmail.com>
2025-09-29 23:31:51 +0100

    Add camera permissions

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,42 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { CameraView, Camera } from "expo-camera";
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { CameraView, Camera, useCameraPermissions } from "expo-camera";
+import {Link, Stack} from "expo-router";
 
 
-const Home = () => {
+export default function  Home() {
+
+    const [permission, requestPermission] = useCameraPermissions();
+    const permissionGranted= Boolean(permission?.granted);
+
+
     return (
-        <View>
+        <View style = {styles.container}>
             <Text>
                 Home
             </Text>
+
+            <Pressable onPress={requestPermission}><Text style = {styles.button}>Request permissions</Text></Pressable>
+            <Link href={"/scanner"} as Child>
+                <Pressable disabled ={!permissionGranted}>
+                    <Text style = {[styles.button, {color: !permissionGranted ? "red": "green"}]}>ScanCode</Text>
+                </Pressable>
+            </Link>
         </View>
     )
 }
 
-export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 80,
+    },
+    button: {
+        color: "green",
+        fontSize: 17,
+        textAlign: "center",
+    }
+})
