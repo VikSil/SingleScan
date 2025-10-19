6f3010b5a5165df27b3d2964be0fd83792412097
Viktoria Silver <viktoria.sudraba@gmail.com>
2025-10-19 02:48:13 +0100

    Update World of Books label

--- app/scanner.jsx
+++ app/scanner.jsx
@@ -595,7 +595,7 @@ export default function Scanner() {
             adjustsFontSizeToFit={true}
             style={styles.vendor}
           >
-            Sell Your Books
+            World of Books
           </Text>
           <Text style={[styles.offer, styles.splitOffer]}>{ziffitOffer}</Text>
           <Text style={[styles.offer, styles.dash]}>{ziffitDash}</Text>
